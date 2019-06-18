9742a9bfab2e1ff16f61c5fe84513bc55b13ab5c
SF92 <ScottJFraser@googlemail.com>
2019-06-18 16:23:15 +0100

    added UserSignUp view

--- user_management/urls.py
+++ user_management/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import url, include
 # TEMPLATE TAGGING
 app_name = "user_management"
 
-urlpatterns = [url("UserLogin/", views.UserLogin.as_view(), name="UserLogin")]
+urlpatterns = [
+    url("UserLogin/", views.UserLogin.as_view(), name="UserLogin"),
+    url("UserSignUp/", views.UserSignUp.as_view(), name="UserSignUp"),
+]
 
--- user_management/views.py
+++ user_management/views.py
@@ -12,6 +12,8 @@ from rest_framework.views import APIView
 
 from .serializers import LoginSerializer
 
+from user_management.models import UserInfo
+
 
 class UserLogin(APIView):
     def post(self, request):
@@ -26,3 +28,43 @@ class UserLogin(APIView):
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key}, status=200)
 
+
+class UserSignUp(APIView):
+    authentication_classes = (TokenAuthentication,)
+
+    def post(self, request):
+
+        password = request.data["password"]
+        email = request.data["username"]
+        email = email.lower()
+        username = email
+
+        if UserModel.objects.filter(email=email):
+            # Email address belongs to registered user
+            msg = "Email address belongs to registered user"
+            return Response(msg, status=400)
+
+        elif UserModel.objects.filter(username=username):
+            # Email address belongs to registered user
+            msg = "username belongs to registered user"
+            return Response(msg, status=400)
+
+        else:
+            newUser = UserModel(
+                username=email, email=email, password=password, is_active=True
+            )
+            newUser.set_password(newUser.password)
+            newUser.save()
+
+            new_UserInfo = UserInfo(user=newUser)
+            new_UserInfo.save()
+
+            user = authenticate(username=username, password=password)
+            if user:
+                django_login(request, user)
+            else:
+                msg = "error"
+                return Response(msg, status=400)
+
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=200)
